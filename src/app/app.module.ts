24f96c9652a7df78a004f3982b8fa635aab89d01
anandprajapati1 <apraj2@sapient.com>
2017-10-03 13:28:25 +0530

    Added Change password functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,10 @@ import { HttpModule } from '@angular/http'
 import { AppRoutingModule } from '../app/app-routing.module'
 import './rxjs-extensions';
 
+// Service import
 import { DataService } from '../app/services/app.service';
 import { SharedService } from '../app/services/shared-service.service';
+// Components import
 import { AppComponent } from './app.component';
 import { ThankYouComponent } from './thankyou.component';
 import { HomeComponent } from './home/home.component';
@@ -14,10 +16,12 @@ import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ThankYouComponent, HomeComponent, LoginComponent, HeaderComponent, MyOrdersComponent, RegisterComponent
+    AppComponent, ThankYouComponent, HomeComponent, LoginComponent, HeaderComponent, MyOrdersComponent, RegisterComponent, ProfileComponent, ChangePasswordComponent
   ],
   imports: [
     BrowserModule, FormsModule, ReactiveFormsModule, HttpModule, AppRoutingModule
--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -3,116 +3,125 @@ import { Http, Headers, Response } from '@angular/http';
 
 import { Observable } from 'rxjs'
 import 'rxjs/add/operator/toPromise';
-import { MealOptions, Order, UserOrders, registrationData, loginData, loginResponse, registrationResponse } from '../model/app.modelClasses';
+import * as _MC from '../model/app.modelClasses';
 
 @Injectable()
 export class DataService {
-	private baseUrl = 'http://localhost:3000';  // URL to web api
-	private _headers = new Headers({ 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'localhost:*' });
-	private isUserLoggedInKey: string = "_iuli";
-	private lastLoginDateKey: string = "lastLogin";
+    // private baseUrl = 'https://63f743eb.ngrok.io';  // URL to web api
+    private baseUrl = 'http://localhost:3000';  // URL to web api
+    private _headers = new Headers({ 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'localhost:*' });
+    private isUserLoggedInKey: string = "_iuli";
+    private lastLoginDateKey: string = "lastLogin";
 
-	constructor(private http: Http) { }
+    constructor(private http: Http) { }
 
-	getCurrentUserId(): string {
-		return localStorage.getItem(this.isUserLoggedInKey) || sessionStorage.getItem(this.isUserLoggedInKey)
-	}
-	isLoggedIn(): boolean {
-		let today: Date = new Date();
-		today.setDate(today.getDate() - 30);
-		let lastLoginDate: Date = localStorage.getItem(this.lastLoginDateKey) && new Date(localStorage.getItem(this.lastLoginDateKey));
+    getCurrentUserId(): string {
+        return localStorage.getItem(this.isUserLoggedInKey) || sessionStorage.getItem(this.isUserLoggedInKey)
+    }
+    isLoggedIn(): boolean {
+        let today: Date = new Date();
+        today.setDate(today.getDate() - 30);
+        let lastLoginDate: Date = localStorage.getItem(this.lastLoginDateKey) && new Date(localStorage.getItem(this.lastLoginDateKey));
 
-		//>> log out if 30 days passed since last login
-		if (lastLoginDate && today > lastLoginDate) {
-			this.logoutUser()
-			return false
-		}
-		else if (localStorage.getItem(this.isUserLoggedInKey) || sessionStorage.getItem(this.isUserLoggedInKey)) {
-			return true;
-		}
-		return false;
-	}
+        //>> log out if 30 days passed since last login
+        if (lastLoginDate && today > lastLoginDate) {
+            this.logoutUser()
+            return false
+        }
+        else if (localStorage.getItem(this.isUserLoggedInKey) || sessionStorage.getItem(this.isUserLoggedInKey)) {
+            return true;
+        }
+        return false;
+    }
 
-	logoutUser() {
-		sessionStorage.removeItem(this.isUserLoggedInKey)
-		localStorage.removeItem(this.isUserLoggedInKey)
-		localStorage.removeItem(this.lastLoginDateKey)
-	}
+    logoutUser() {
+        sessionStorage.removeItem(this.isUserLoggedInKey)
+        localStorage.removeItem(this.isUserLoggedInKey)
+        localStorage.removeItem(this.lastLoginDateKey)
+    }
 
-	createLoginSession(_userid: string, _remember: boolean) {
-		// iuli => is user logged in
-		if (_remember) {
-			localStorage.setItem(this.isUserLoggedInKey, _userid)
-			localStorage.setItem(this.lastLoginDateKey, (new Date()).toUTCString())
-		}
-		else {
-			sessionStorage.setItem(this.isUserLoggedInKey, _userid)
-		}
-	}
+    createLoginSession(_userid: string, _remember: boolean) {
+        // iuli => is user logged in
+        if (_remember) {
+            localStorage.setItem(this.isUserLoggedInKey, _userid)
+            localStorage.setItem(this.lastLoginDateKey, (new Date()).toUTCString())
+        }
+        else {
+            sessionStorage.setItem(this.isUserLoggedInKey, _userid)
+        }
+    }
 
-	loginUser(_logindata: loginData): Promise<loginResponse> {
-		return this.http.post(`${this.baseUrl}/loginUser`, _logindata, { headers: this._headers }).toPromise()
-			.then(x => {
-				let res = x.json() as loginResponse;
-				this.createLoginSession(res.userId, _logindata.rememberMe);
-				return res;
-			})
-			.catch(this.handleError);
-	}
+    loginUser(_logindata: _MC.loginData): Promise<_MC.loginResponse> {
+        return this.http.post(`${this.baseUrl}/loginUser`, _logindata, { headers: this._headers }).toPromise()
+            .then(x => {
+                let res = x.json() as _MC.loginResponse;
+                if (res.isUsernameValid && res.isPasswordValid)
+                    this.createLoginSession(res.userId, _logindata.rememberMe);
+                return res;
+            })
+            .catch(this.handleError);
+    }
 
-	registerUser(userdata: registrationData): Promise<registrationResponse> {
-		const url = `${this.baseUrl}/registerUser`;
-		return this.http.post(url, userdata, { headers: this._headers }).toPromise()
-			.then(x => {
-				if (x != null) {
-					let res = x.json() as registrationResponse;
-					if (res.isRegistered)
-						this.createLoginSession(res.userId, false);
-					return res;
-				}
-			}).catch(
-			this.handleError
-			);
-	}
+    registerUser(userdata: _MC.registrationData): Promise<_MC.registrationResponse> {
+        const url = `${this.baseUrl}/registerUser`;
+        return this.http.post(url, userdata, { headers: this._headers }).toPromise()
+            .then(x => {
+                if (x != null) {
+                    let res = x.json() as _MC.registrationResponse;
+                    if (res.isRegistered)
+                        this.createLoginSession(res.userId, false);
+                    return res;
+                }
+            }).catch(
+            this.handleError
+            );
+    }
 
-	getMealOptions(): Promise<MealOptions[]> {
-		return this.http.get(`${this.baseUrl}/getMealOptions`, { headers: this._headers }).toPromise()
-			.then(response => response.json() as MealOptions[])
-			.catch(this.handleError);
-	}
+    changePassword(_data: _MC.changePasswordData): Promise<boolean> {
+        const url = `${this.baseUrl}/changePassword`;
+        return this.http.post(url, _data, { headers: this._headers }).toPromise()
+            .then(x => x.json())
+            .catch(this.handleError)
+    }
 
-	placeOrder(order: Order): Promise<Order> {
-		const url = `${this.baseUrl}/placeOrder`;
-		return this.http.post(url, order, { headers: this._headers }).toPromise()
-			.then(res => {
-				if (res != null) {
-					// console.log(res);
-					return res.json() as Order;
-				}
-			}
-			).catch(this.handleError);
-	}
+    getMealOptions(): Promise<_MC.MealOptions[]> {
+        return this.http.get(`${this.baseUrl}/getMealOptions`, { headers: this._headers }).toPromise()
+            .then(response => response.json() as _MC.MealOptions[])
+            .catch(this.handleError);
+    }
 
-	cancelOrder(orderid: string): Promise<boolean> {
-		const url = `${this.baseUrl}/cancelOrder`;
-		return this.http.post(url, { id: orderid }, { headers: this._headers }).toPromise()
-			.then(res => {
-				if (res != null) {
-					// console.log(res);
-					return res.json() as boolean;
-				}
-			}
-			).catch(this.handleError);
-	}
+    placeOrder(order: _MC.Order): Promise<_MC.Order> {
+        const url = `${this.baseUrl}/placeOrder`;
+        return this.http.post(url, order, { headers: this._headers }).toPromise()
+            .then(res => {
+                if (res != null) {
+                    // console.log(res);
+                    return res.json() as _MC.Order;
+                }
+            }
+            ).catch(this.handleError);
+    }
 
-	getUserOrderList(userid: string): Promise<UserOrders[]> {
-		return this.http.get(`${this.baseUrl}/getUserOrders/${userid}`, { headers: this._headers }).toPromise()
-			.then(res => res.json() as UserOrders[])
-			.catch(this.handleError);
-	}
+    cancelOrder(orderid: string): Promise<boolean> {
+        const url = `${this.baseUrl}/cancelOrder`;
+        return this.http.post(url, { id: orderid }, { headers: this._headers }).toPromise()
+            .then(res => {
+                if (res != null) {
+                    // console.log(res);
+                    return res.json() as boolean;
+                }
+            }
+            ).catch(this.handleError);
+    }
 
-	private handleError(error: any): Promise<any> {
-		console.error('An error occurred', error);
-		return Promise.reject(error.message || error);
-	}
+    getUserOrderList(userid: string): Promise<_MC.UserOrders[]> {
+        return this.http.get(`${this.baseUrl}/getUserOrders/${userid}`, { headers: this._headers }).toPromise()
+            .then(res => res.json() as _MC.UserOrders[])
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
+    }
 }
